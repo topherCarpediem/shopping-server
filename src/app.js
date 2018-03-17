a5e65d44a48f39f5bd0833d590106834b770920d
Christopher Enriquez <topher.carpediem@gmail.com>
2018-03-17 17:19:36 +0800

    Added pagination

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import express from "express";
 import { userAPI } from "./components/User";
 import productAPI from "./components/Product/ProductAPI"
 
-global.__basedir = __dirname;
+global.__imageLink = "http://127.0.0.1:3001/product/images/";
 
 const app = express()
 
+
+import compression from "compression";
+app.use(compression())
 app.use('/user', userAPI)
 app.use('/product', productAPI)
 
--- src/components/Product/ProductAPI.js
+++ src/components/Product/ProductAPI.js
@@ -12,10 +12,8 @@ const { Product, Category } = model
 const upload = multer({ dest: 'uploads/' })
 const Products = Router();
 
+// Add products to the database
 Products.post("/add", tokenValidation, upload.single('avatar'), (req, res) => {
-    // console.log(req.id)
-    // console.log(req.file)
-
     
     let old = path.join(process.cwd(), req.file.path)
     fs.renameSync(old, old+".png")
@@ -27,7 +25,7 @@ Products.post("/add", tokenValidation, upload.single('avatar'), (req, res) => {
         productDescription: "The sample description",
         imageCover: `${req.file.filename}.png`,
         isActive: true,
-        category_id: "6853bd35-2a2d-4557-a9c5-c213aae96e55",
+        
         user_id: req.id
     }
 
@@ -40,24 +38,72 @@ Products.post("/add", tokenValidation, upload.single('avatar'), (req, res) => {
     }))
 })
 
-Products.get("/image", (req, res) => {
-    Product.findAll().then(res => {
-        return res.map(product => {
-            product.dataValues.imageCover = `https://127.0.0.1:3001/product/images/${product.dataValues.imageCover}`
-            return product.dataValues
-        })
-    }).then(product => {
-        res.setHeader("Content-type", "application/json")
-        res.status(200).end(JSON.stringify(product))
+// ======================================================
+// * * * Retrive product with pagination technique. * * * 
+// ======================================================
+Products.get("/page/:pagenumber", (req, res) => {
+    const { pagenumber } = req.params
+    const pageOffset = pagenumber > 1 ? pagenumber * 20 : 0
+    
+    Product.findAll({
+        order: [
+            ['created_at', 'DESC'],
+        ],
+        offset: pageOffset,
+        limit: 20,
+    }).then(result => {
+        let dataValues = null
+
+        if(result.length === 0){
+            dataValues = {
+                message: "Page not found."
+            }
+            res.setHeader("Content-type", "application/json")
+            res.status(404).end(JSON.stringify(dataValues))
+        } else {
+            dataValues = result.map(productItem => {
+                productItem.dataValues.imageCover = `${__imageLink}${productItem.dataValues.imageCover}`
+                return productItem.dataValues
+            });
+            res.setHeader("Content-type", "application/json")
+            res.status(200).end(JSON.stringify(dataValues))
+        }
     })
-    //fs.createReadStream(path.join(process.cwd(), "uploads\\f5ed7a402e252f6d5dc2ddf5888393b1"+".png")).pipe(res)
+})  
+
+
+// ======================================================
+// * * * * * * * * * * Download image * * * * * * * * * *
+// ======================================================
+Products.get("/images/:imagename", (req, res) => {
+    
+        let filePath = path.join(process.cwd(), `uploads\\${req.params.imagename}`)
+        const myReadStream = fs.createReadStream(filePath)    
+
+        myReadStream.on("error", error => {
+            console.log("Returning invalid image because image not found")
+            filePath = path.join(process.cwd(), `src\\images\\error.PNG`)
+            fs.createReadStream(filePath).pipe(res)
+        })
+
+        myReadStream.pipe(res)
 })
 
 
 
 function tokenValidation(req, res, next){
     let validationResult = null
-    const authHeader = req.headers.authorization.split(" ")
+    let authHeader = null
+
+    try {
+        authHeader =  req.headers.authorization.split(" ")
+    } catch (error) {
+        res.setHeader('Content-Type', 'application/json')
+        res.status(401).end(JSON.stringify({
+            message: "Unauthorized request",
+            reason: "Unable to detect the authorization token"
+        })) 
+    }
 
     if(authHeader.length != 2 || authHeader[0] != "Bearer") {
         res.setHeader('Content-Type', 'application/json')
